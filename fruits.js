7c1e0cf4888b12a88c2267b78f096cb71d5d3ef1
DanielMakokha <danielmakokhan@gmail.com>
2023-08-31 05:47:00 +0300

    new-update

---
+++ fruits.js
@@ -0,0 +1,21 @@
+2+3
+3-5
+3*4
+21/3
+21//3
+"hello" + ' world'
+"hello" + " world"
+console
+console.log("Hi")
+let age=30
+age
+age*2
+age*2-3
+const add3=(num)=>{
+return a+b;}
+_
+Math.random(10)
+Math.random(10)
+console.log("Hi")
+
+fruits=['Banana','apples','Pinneapple']
